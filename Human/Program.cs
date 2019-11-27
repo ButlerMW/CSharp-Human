53771a8c68fc057436c520e3f26fc5ab4ae334cf
ButlerMW <57372823+ButlerMW@users.noreply.github.com>
2019-11-27 10:39:20 -0700

    Add files via upload

---
+++ Human/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Human
+{
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int Health {get;set;}
+        
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            Health = 100;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = health;
+        }
+        // add a public "getter" property to access health
+        //ass a constructor thaty takes a value to se NAme, and set the remaining fields to default values
+        //add a constructor to assign custon values to all feilds
+        //build attack method
+        public int Attack(Human target)
+        {
+            target.Health -= 5 * Strength;
+            Console.WriteLine($"{target.Name} loses {5 * Strength} Health!");
+            return target.Health;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Human daryl = new Human("Daryl");
+            Console.WriteLine(daryl);
+            daryl.Attack(daryl);
+        }
+    }
+}
